916154c4c43afb141ea2ba28b8093f513a7c4206
argkmr <125196042+argkmr@users.noreply.github.com>
2024-03-07 02:31:47 +0530

    added feature to redirect to homepage on reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import './App.scss'
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+import { useEffect } from 'react'
 
 const App = () => {
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    const navigationEntry = performance.getEntriesByType("navigation")[0] as PerformanceNavigationTiming;
+    if (navigationEntry && navigationEntry.type === 'reload') {
+      navigate('/');
+    }
+  }, [navigate]);
+
   return (
     <>
       <NavBar />
